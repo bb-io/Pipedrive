e9650539f694444c317c9eb6f0a203281074eacd
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:50:18 +0300

    Categories (#7)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * added static inputs
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>

--- Apps.Pipedrive/PipedriveApplication.cs
+++ Apps.Pipedrive/PipedriveApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Pipedrive;
 
-public class PipedriveApplication : IApplication
+public class PipedriveApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.Crm];
+        set { }
+    }
+    
     public T GetInstance<T>()
     {
         throw new NotImplementedException();
